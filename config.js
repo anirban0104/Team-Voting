24cbf861bd2897a877a73efe92231bc77b97f2bc
Anirban Biswas <68367011+anirban0104@users.noreply.github.com>
2021-09-24 20:58:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  
+  apiKey: "AIzaSyBXd9NirRBC_vrlFbMrEQWoP6cJlbVD39w",
+  authDomain: "project-c67.firebaseapp.com",
+  databaseURL: "https://project-c67-default-rtdb.firebaseio.com",
+  projectId: "project-c67",
+  storageBucket: "project-c67.appspot.com",
+  messagingSenderId: "1045474023513",
+  appId: "1:1045474023513:web:2f13b8a2f228fcd6ea7948"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
